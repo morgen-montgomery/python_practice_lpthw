814300d412e2bc02416fbe1dc781debb4e157668
Morgen Fett <mfett004@gmail.com>
2016-11-03 14:54:39 -0700

    next exercise done

---
+++ ex34.py
@@ -0,0 +1,14 @@
+1- python
+2- peacock
+3- bear
+4- kangaroo
+5- whale
+6- peacock
+7- platypus
+8- whale
+
+# SD
+# I dont understand this:
+# With what you know of the difference between these types of numbers,
+# can you explain why the year 2010 in "January 1, 2010," really is
+# 2010 and not 2009? (Hint: you can't pick years at random.)
